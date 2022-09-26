1ea9005640a25ebe70045cd1aeecb3480ba7f9de
gominnam <kosok03@naver.com>
2022-09-26 19:47:12 +0900

    init toy-react-jave project

---
+++ src/test/java/com/web/toyreactjava/ToyReactJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.toyreactjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyReactJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
