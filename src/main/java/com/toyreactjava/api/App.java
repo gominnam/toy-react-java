594c7b90d0c51bcec43a6a08af7965b8a42068d6
gominnam <kosok03@naver.com>
2022-10-15 11:53:19 +0900

    add CORS and mui

--- src/main/java/com/toyreactjava/api/App.java
+++ src/main/java/com/toyreactjava/api/App.java
@@ -2,7 +2,10 @@ package com.toyreactjava.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -10,5 +13,15 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
