436a0a0370ba72ebb936365d4932cc680832aa42
gominnam <kosok03@naver.com>
2022-10-25 23:11:20 +0900

    add fontawesome

--- src/main/frontend/src/components/Board.js
+++ src/main/frontend/src/components/Board.js
@@ -2,14 +2,34 @@ import React, {useState, useEffect} from 'react';
 import {useNavigate} from "react-router-dom"
 import axios from "axios";
 import {
-    Box,
-    Grid
+    Box, Collapse,
+    Grid, Icon, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader
 } from "@mui/material";
 import Button from '@material-ui/core/Button';
+import {ExpandLess, ExpandMore, StarBorder} from "@mui/icons-material";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {fa1} from "@fortawesome/free-solid-svg-icons";
+
+function SendIcon() {
+    return null;
+}
+
+function DraftsIcon() {
+    return null;
+}
+
+function InboxIcon() {
+    return null;
+}
 
 const Board = () => {
     const navigate = useNavigate();
     const [rankItems, setRankItems] = useState([]);
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
 
     useEffect(() => {
         axios.get('/api/real-time')
@@ -28,23 +48,57 @@ const Board = () => {
 
     return (
         <>
-            <h1 style={{color:"green"}}>네이버 실시간 검색어</h1>
+        <List
+            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+                <ListSubheader component="div" style={{fontSize: "25px", color:"green", textAlign:"center"}} id="nested-list-subheader">
+                    네이버 실시간 검색어
+                </ListSubheader>
+            }
+        >
+            {rankItems.map(rank => (
+                <ListItemButton component="a" href={rank.href}>
+                    <Icon>
+                        <FontAwesomeIcon icon={fa1} />
+                    </Icon>
+                    <ListItemText primary={rank.title} />
+                </ListItemButton>
 
-            <div className="rank-column">
-                {rankItems.map(rank => (
-                    <div>
-                        <a href={rank.href} className="rank-layer">
-                        <Box component="span" className="rank-num">{rank.no}</Box>
-                        <span className="rank-title">{rank.title}</span>
-                        </a>
-                    </div>
-                ))}
-            </div>
+            ))}
 
-            <Button onClick={()=>navigate(-1)} variant="contained" color="primary">
-                Go Back Page
-            </Button>
-        </>
+            <ListItemButton>
+                <ListItemIcon>
+                    <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Drafts" />
+            </ListItemButton>
+            <ListItemButton onClick={handleClick}>
+                <ListItemIcon>
+                    <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemIcon>
+                            <StarBorder />
+                        </ListItemIcon>
+                        <ListItemText primary="Starred" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
+        </List>
+
+
+
+        <Button onClick={()=>navigate(-1)} variant="contained" color="primary">
+            Go Back Page
+        </Button>
+    </>
     )
 };
 
